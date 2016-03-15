420544684575f4f2932555a6a9f0fa059fda61de
m.jabbari123@gmail.com <max000dj>
2016-03-15 11:14:10 +0400

    Initial check-in of module Qmunicate.Xamarin

---
+++ Qmunicate.Xamarin/ApplicationKeys.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Qmunicate.Xamarin
+{
+	public static class ApplicationKeys
+	{
+		//clients = new QuickbloxClient(36130, "f9xr5MmyrKOPnaW", "ueZm2PNM676OkO6", null);
+
+
+//		public const int ApplicationId = 13318;
+//		public const string AuthorizationKey = "WzrAY7vrGmbgFfP";
+//		public const string AuthorizationSecret = "xS2uerEveGHmEun";
+//
+
+
+		public const int ApplicationId = 36130;
+		public const string AuthorizationKey = "f9xr5MmyrKOPnaW";
+		public const string AuthorizationSecret = "ueZm2PNM676OkO6";
+		public const string ApiBaseEndpoint = "http://api.quickblox.com";
+		public const string ChatEndpoint = "chat.quickblox.com";
+
+		public const long FbAppId = 605405446247805;
+		public const string FbScope = "email,user_friends";
+
+	}
+}
+
